559e3870e22f5fdd0894054a63f7241f3e84c926
h_ly <h_ly@brycen.com.vn>
2025-04-23 14:04:58 +0700

    Init commit

---
+++ src/main/angular/src/app/checkout/checkout.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../models/product.model';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+
+export class CheckoutComponent {
+
+  products: Product[] = [];
+  cart: any[] = [];
+  customerName: string = '';
+  address: string = '';
+
+  ngOnInit(): void {
+    this.http.get<any>('http://localhost:8085/api/products')
+      .subscribe(response => {
+        if (response.status === 200) {
+          this.products = response.data;  // Lấy dữ liệu sản phẩm từ response
+        } else {
+          // Xử lý khi có lỗi (nếu cần)
+          console.error('Lỗi: ', response.message);
+        }
+      });
+  }
+
+  constructor(private http: HttpClient) {}
+
+  addToCart(product: any) {
+    const existing = this.cart.find(item => item.name === product.name);
+    if (existing) {
+      existing.quantity++;
+    } else {
+      this.cart.push({ ...product, quantity: 1 });
+    }
+  }
+
+  getTotal() {
+    return this.cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  }
+
+  checkout() {
+    const payload = {
+      customerName: this.customerName,
+      address: this.address,
+      items: this.cart
+    };
+
+    this.http.post<any>('http://localhost:8085/api/payment/create', payload)
+    .subscribe(res => {
+      if (res && res.payUrl) {
+        window.location.href = res.payUrl;
+      }
+    });
+  }
+}
