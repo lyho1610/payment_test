4781c5b2f753f0b547d700e40a08c158e89de839
h_ly <h_ly@brycen.com.vn>
2025-04-25 17:41:32 +0700

    fix url call api backend environment production

--- src/main/angular/src/environments/environment.prod.ts
+++ src/main/angular/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://paymenttest-production-858f.up.railway.app/api'
+  apiUrl: 'https://paymenttest-production-858f.up.railway.app:8085/api'
 };
